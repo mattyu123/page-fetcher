4fb02aadc203e7f874c38aa6da0121a46da7d269
mattyu123 <matthewyu100@gmail.com>
2023-05-27 14:50:51 -0400

    run code through linter

--- fetcher.js
+++ fetcher.js
@@ -1,22 +1,23 @@
-//Saves the command line arguements into variables 
-const url = `http://${process.argv[2]}`
-const localFilePath = process.argv[3]
+//Saves the command line arguements into variables
+const url = `http://${process.argv[2]}`;
+const localFilePath = process.argv[3];
 
 // //set up basic client code connection and write a file
 const fs = require('fs');
 const request = require('request');
 
 // //Create the connection with the url provided in the command line
-request(url, (error, response, body) => {  
+request(url, (error, response, body) => {
+  //prints the error if there is an error
   if (error) {
-    console.log('Error:', error); // Print the error if one occurred
+    console.log('Error', error.message);
   }
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   
   //Write what you get from the request to the specified file path
   fs.writeFile(localFilePath, body, err => {
-  if (err) {
-    console.error(err);
+    if (err) {
+      console.error(err);
     }
   });
 });
