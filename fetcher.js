18b0499005d2f4010068085a5e0cee1021851e84
mattyu123 <matthewyu100@gmail.com>
2023-05-27 13:48:19 -0400

    write some content to a file

--- fetcher.js
+++ fetcher.js
@@ -1,30 +1,43 @@
 //Saves the command line arguements into variables 
-const url = process.argv[2]
-const localFilePath = process.argv[3]
+// const url = process.argv[2]
+// const localFilePath = process.argv[3]
 
-//set up basic client code connection and write a file
-const net = require('net');
+// //set up basic client code connection and write a file
+// const net = require('net');
 const fs = require('fs');
 
-//Create the connection with the url provided in the command line
-const conn = net.createConnection({ 
-  host: url,
-  port: 80
+// //Create the connection with the url provided in the command line
+// const conn = net.createConnection({ 
+//   host: url,
+//   port: 80
+// });
+
+// conn.setEncoding('UTF8');
+
+//attempting to write the content to the file
+const content = 'This is some new and updated content';
+
+fs.writeFile('/Users/mattyu/Desktop/test/test.txt', content, err => {
+  if (err) {
+    console.error(err);
+  }
+  // file written successfully
 });
 
-conn.setEncoding('UTF8');
 
-//Sends a request over to the server 
-conn.on('connect', () => {
-  console.log(`Connected to server!`);
 
-  conn.write(`GET / HTTP/1.1\r\n`);
-  conn.write(`Host: ${url}\r\n`);
-  conn.write(`\r\n`);
-});
+
+//Sends a request over to the server -- This code we get will return the index.html code 
+// conn.on('connect', () => {
+//   console.log(`Connected to server!`);
+
+//   conn.write(`GET / HTTP/1.1\r\n`);
+//   conn.write(`Host: ${url}\r\n`);
+//   conn.write(`\r\n`);
+// });
 
 //This will take the data that we receive from the server and console.log it 
-conn.on('data', (data) => {
-  console.log(data);
-  conn.end();
-});
+// conn.on('data', (data) => {
+//   console.log(data);
+//   conn.end();
+// });
